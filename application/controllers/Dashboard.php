22251949fdc9322075e30176086fa127d6f1d5b8
Apip-Luki <alphatesterofficial@gmail.com>
2020-10-09 17:02:02 +0700

    perubahan

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,2 +1,9 @@
 <?php
 defined('BASEPATH') or exit('No direct access script allowed');
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
